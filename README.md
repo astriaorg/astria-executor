058bebc748142951e4fa3f60b5dd558987261612
Jesse Snyder <jessetsnyder@gmail.com>
2023-03-14 11:58:00 -0600

    dont need golang items

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Astria Executor
 
 Provides an RPC server for Astria packages to interface with the EVM.
+
+Running for development
+
+* run `buf generate` to generate necessary client files
+
+* run `cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use color_eyre::eyre::Result;
+
 use astria_executor::execution_server::execution::execution_service_server::ExecutionServiceServer;
 use astria_executor::execution_server::ExecutionRpcServer;
 use tonic::transport::Server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let address = "[::1]:50051".parse()?;
     let server = ExecutionRpcServer::default();
     Server::builder()
